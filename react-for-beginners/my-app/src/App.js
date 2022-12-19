a50e61fb10854153d89708b0ab5207cdf7a3febe
choizlor <choizlor@gmail.com>
2022-12-19 23:54:49 +0900

    todo

--- react-for-beginners/my-app/src/App.js
+++ react-for-beginners/my-app/src/App.js
@@ -11,12 +11,12 @@ function App() {
     if (toDo === "") {
       return;
     }
-    setToDo("");
     setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
   };
   return (
     <div>
-      <h1>My To Dos</h1>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
@@ -26,6 +26,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
