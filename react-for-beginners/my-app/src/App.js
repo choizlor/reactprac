1dd1bffc665a2c1c4ef3f822218f4ad1851d0041
choizlor <choizlor@gmail.com>
2022-12-22 23:14:52 +0900

    movie app part 1

--- react-for-beginners/my-app/src/App.js
+++ react-for-beginners/my-app/src/App.js
@@ -2,54 +2,39 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [cost, setCost] = useState(1);
-  const [need, setNeed] = useState(1);
-  const onChange = (event) => {
-    setCost(event.target.value);
-  };
-  const costInput = (event) => {
-    setNeed(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onChange}>
-          <option>Select Coin</option>
-          {coins.map((coin, index) => (
-            <option
-              key={index}
-              value={coin.quotes.USD.price}
-              id={coin.symbol}
-              symbol={coin.symbol}
-            >
-              {coin.name}({coin.symbol}): ${Math.round(coin.quotes.USD.price*100)/100} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <hr></hr>
-      <h2>Input your cash.</h2>
-      <div>
-        <input
-          type="number"
-          value={need}
-          onChange={costInput}
-          placeholder="BTC"
-        />
-      </div>
-      <h2>You can get {Math.round((need / cost)*100)/100}</h2>
     </div>
   );
 }
