4905a7c199e029e7e719b8e47256ad1963d3e8b9
choizlor <choizlor@gmail.com>
2022-12-23 23:59:14 +0900

    movie app ing

---
+++ react-for-beginners/my-app/src/Movie.js
@@ -0,0 +1,15 @@
+function Movie() {
+  return (
+    <div>
+      <img src={medium_cover_image} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Movie;
