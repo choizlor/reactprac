d40e510ecaf3376366bb0511c55174c65fa50c6d
choizlor <choizlor@gmail.com>
2022-12-31 23:35:28 +0900

    드림코딩 4챕터 완

--- projects/react/basic/src/index.js
+++ projects/react/basic/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AppProducts from './AppProducts';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppCounter from './AppCounter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppCounter />
+    <AppProducts />
   </React.StrictMode>
 );
 
