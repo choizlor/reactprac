8198e61d114be39e0ec7db4845ccafd8bb9a8f67
choizlor <choizlor@gmail.com>
2023-01-04 23:56:48 +0900

    드림코딩 5.6

--- projects/react/basic/src/AppMentor.jsx
+++ projects/react/basic/src/AppMentor.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function AppMentor(props) {
   const [person, setPerson] = useState({
-    name: '엘리',
-    title: '개발자',
+    name: "엘리",
+    title: "개발자",
     mentor: {
-      name: '밥',
-      title: '시니어개발자',
+      name: "밥",
+      title: "시니어개발자",
     },
   });
   return (
@@ -20,6 +20,10 @@ export default function AppMentor(props) {
       <button
         onClick={() => {
           const name = prompt(`what's your mentor's name?`);
+          setPerson((person) => ({
+            ...person,
+            mentor: { ...person.mentor, name },
+          }));
         }}
       >
         멘토 이름 바꾸기
@@ -27,6 +31,10 @@ export default function AppMentor(props) {
       <button
         onClick={() => {
           const title = prompt(`what's your mentor's title?`);
+          setPerson((person) => ({
+            ...person,
+            mentor: { ...person.mentor, title },
+          }));
         }}
       >
         멘토 타이틀 바꾸기
