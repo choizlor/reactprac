910bc733b786928a9ea30e3c0bc97f0910994148
choizlor <choizlor@gmail.com>
2023-01-08 23:57:05 +0900

    ing

--- projects/react/basic/src/AppCard.jsx
+++ projects/react/basic/src/AppCard.jsx
@@ -6,12 +6,10 @@ export default function AppCard() {
       <Card>
         <p>Card1</p>
       </Card>
-
       <Card>
         <h1>Card2</h1>
         <p>설명</p>
       </Card>
-
       <Card>
         <article></article>
       </Card>
