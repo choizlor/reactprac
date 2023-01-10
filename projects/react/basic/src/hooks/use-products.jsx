7bad753c5197c4634a14ecfb2a69f63613b84bc2
choizlor <choizlor@gmail.com>
2023-01-11 00:29:33 +0900

    드림코딩 5장 끝

---
+++ projects/react/basic/src/hooks/use-products.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export default function useProducts({ salesOnly }) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+    setError(undefined);
+    fetch(`data/${salesOnly ? "sale_" : ""}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("🔥뜨끈한 데이터를 네트워크에서 받아옴");
+        setProducts(data);
+      })
+      .catch((e) => setError("에러가 발생"))
+      .finally(() => setLoading(false));
+    return () => {
+      console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
+    };
+  }, [salesOnly]);
+
+  return [loading, error, products];
+}
+
+// Hooks는 값의 재사용이 아니라 로직의 재사용을 위한 것
